e0ff1158ea248f3f54e7a421cd70ce86b9582006
Christopher Olson <50605567+severus-piton@users.noreply.github.com>
2022-03-03 18:36:28 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,20 @@
+# Write your code below this line 👇
+""" Part 1
+a = input('What is your name?  ')
+print('Hello, ' + a + '!')
+print('There are ' + str(len(a)) + ' letters in your name.')
+"""
+
+""" Part 2
+a = input("a: ")
+b = input("b: ")
+c = a
+a = b
+b = c
+print("a: " + a)
+print("b: " + b)
+"""
+
+#""" Part 3
+two_digit_number = input("Type a two digit number: ")
+#"""
